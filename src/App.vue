fba56a02e19a909425ca28ccd91a315548057b06
Sascha Hanse <shanse@posteo.de>
2017-10-04 02:32:33 +0200

    enables token transfer between identities

--- src/App.vue
+++ src/App.vue
@@ -35,25 +35,30 @@
       <button v-on:click="generateAddress">Generate Identity</button>
       <ul>
         <li v-for="(addr, idx) in addrList" v-bind:class="{ 'active-addr': addrIdx === idx}">
-          {{ addr }} has {{ tokens[idx] }} AE <button @click="selectAddress(idx)">select</button>
+          {{ addr }} has {{ tokenDisplay[idx] }} AE <button @click="selectAddress(idx)">select</button>
+          <input v-model="tokenTransferValue" type="number" min="0.000000000000000001" v-if="addrIdx !== idx"><button v-if="addrIdx !== idx" @click="transfer(addr, tokenTransferValue)">send to</button>
         </li>
       </ul>
+    </div>
+    <div>
+      <button v-on:click="logout">logout</button>
     </div>
   </div>
 </template>
 
 <script>
 import lightwallet from 'eth-lightwallet'
+import Transaction from 'ethereumjs-tx'
+import BigNumber from 'bignumber.js'
 import ZeroClientProvider from 'web3-provider-engine/zero'
 import Web3 from 'web3'
-// import RpcSubprovider from 'web3-provider-engine/subproviders/rpc'
 
 export default {
   name: 'app',
-  // components: { Loader },
   computed: {
     seedList: function () { return this.seed.match(/\S+/g) },
-    iframe: function () { return this.iname + '.html' }
+    iframe: function () { return this.iname + '.html' },
+    tokenDisplay: function () { return this.tokens.map(function (e) { return e.toString() }) }
   },
   created: function () {
     if (localStorage.getItem('ks')) {
@@ -67,6 +72,15 @@ export default {
     selectAddress: function (idx) {
       this.addrIdx = idx
     },
+    updateBalances: function () {
+      var that = this
+      for (var i in this.addrList) {
+        that.token.contract.balanceOf(that.addrList[i], function (err, bal) {
+          if (err) throw err
+          that.$set(that.tokens, i, bal.div(that.token.decimals))
+        })
+      }
+    },
     generateAddress: function () {
       if (this.needSetup || this.keystore === undefined) { return }
       var that = this
@@ -77,11 +91,43 @@ export default {
         const off = that.addrList.length - 1
         that.token.contract.balanceOf(that.addrList[off], function (err, bal) {
           if (err) throw err
-          that.$set(that.tokens, off, bal.toString())
-          // [off] = bal.toString()
+          that.$set(that.tokens, off, bal.div(that.token.decimals))
         })
       })
     },
+    transferFrom: function (from, to, amount) {
+      if (this.token.contract === undefined) return
+      var that = this
+      const tVal = this.tokenTransferValue
+      this.token.contract.transfer(to, this.w3.toBigNumber(tVal).mul(this.token.decimals), {from: from, gas: 100000}, function (err, txid) {
+        if (err) {
+          console.log('transferFrom', err)
+          throw err
+        }
+        const fIdx = that.addrList.indexOf(from)
+        const tIdx = that.addrList.indexOf(to)
+        if (fIdx === -1 || tIdx === -1) {
+          console.log('could not update token balances locally')
+          return
+        }
+        // TODO: Check back later if this update actually happened on chain.
+        that.$set(that.tokens, fIdx, that.tokens[fIdx].sub(tVal))
+        that.$set(that.tokens, tIdx, that.tokens[tIdx].add(tVal))
+      })
+    },
+    transfer: function (to, amount) {
+      this.transferFrom(this.addrList[this.addrIdx], to, amount)
+    },
+    logout: function () {
+      this.keystore = {}
+      this.needSetup = true
+      this.providerOpts = {}
+      this.w3 = undefined
+      this.password = ''
+      localStorage.removeItem('ks')
+      this.haveKeyStore = false
+    },
+    setProvider: function () {},
     mkProviderOpts: function () {
       var that = this
       this.keystore.keyFromPassword(that.password, function (err, pwDerivedKey) {
@@ -93,9 +139,9 @@ export default {
             cb(null, [that.addrList[that.addrIdx]])
           },
           signTransaction: function (tx, cb) {
-            var s = lightwallet.txutils.valueTx(tx)
-            console.log('sign', tx, s)
-            var signed = lightwallet.signing.signTx(that.keystore, pwDerivedKey, s, tx.from)
+            const t = new Transaction(tx)
+            console.log('sign', tx, t)
+            var signed = lightwallet.signing.signTx(that.keystore, pwDerivedKey, t.serialize().toString('hex'), tx.from)
             cb(null, '0x' + signed)
           },
           approveTransaction: function (tx, cb) {
@@ -110,32 +156,35 @@ export default {
     initWeb3: function () {
       var that = this
       const abi = JSON.parse('[{"constant":false,"inputs":[],"name":"launch","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[],"name":"creator","outputs":[{"name":"","type":"address"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[],"name":"name","outputs":[{"name":"","type":"string"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"name":"_spender","type":"address"},{"name":"_value","type":"uint256"}],"name":"approve","outputs":[{"name":"success","type":"bool"}],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[],"name":"totalSupply","outputs":[{"name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"name":"_from","type":"address"},{"name":"_to","type":"address"},{"name":"_value","type":"uint256"}],"name":"transferFrom","outputs":[{"name":"success","type":"bool"}],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[],"name":"decimals","outputs":[{"name":"","type":"uint8"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[],"name":"version","outputs":[{"name":"","type":"string"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[{"name":"_owner","type":"address"}],"name":"balanceOf","outputs":[{"name":"balance","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"name":"_addresses","type":"address[]"},{"name":"_values","type":"uint256[]"}],"name":"prefill","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[],"name":"symbol","outputs":[{"name":"","type":"string"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"name":"_to","type":"address"},{"name":"_value","type":"uint256"}],"name":"transfer","outputs":[{"name":"success","type":"bool"}],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":false,"inputs":[{"name":"_spender","type":"address"},{"name":"_value","type":"uint256"},{"name":"_extraData","type":"bytes"}],"name":"approveAndCall","outputs":[{"name":"success","type":"bool"}],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[],"name":"transferableUntil","outputs":[{"name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[{"name":"_owner","type":"address"},{"name":"_spender","type":"address"}],"name":"allowance","outputs":[{"name":"remaining","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[],"name":"prefilled","outputs":[{"name":"","type":"bool"}],"payable":false,"stateMutability":"view","type":"function"},{"inputs":[],"payable":false,"stateMutability":"nonpayable","type":"constructor"},{"payable":false,"stateMutability":"nonpayable","type":"fallback"},{"anonymous":false,"inputs":[{"indexed":true,"name":"_from","type":"address"},{"indexed":true,"name":"_to","type":"address"},{"indexed":false,"name":"_value","type":"uint256"}],"name":"Transfer","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"name":"_owner","type":"address"},{"indexed":true,"name":"_spender","type":"address"},{"indexed":false,"name":"_value","type":"uint256"}],"name":"Approval","type":"event"}]')
-      const opts = {
-        getAccounts: function (cb) {
-          cb(null, that.addrList)
-        },
-        signTransaction: function (tx, cb) {
-          cb(new Error('cannot sign'), null)
-        },
-        approveTransaction: function (tx, cb) {
-          cb(null, false)
-        },
-        rpcUrl: 'https://kovan.infura.io'
-      }
-      this.w3 = new Web3(new ZeroClientProvider(opts))
-      this.w3.eth.contract(abi).at(that.token.address, function (err, contract) {
+      this.keystore.keyFromPassword(that.password, function (err, pwDerivedKey) {
         if (err) throw err
-        that.token.contract = contract
+        const opts = {
+          getAccounts: function (cb) {
+            cb(null, that.addrList)
+          },
+          signTransaction: function (tx, cb) {
+            const t = new Transaction(tx)
+            console.log('sign', tx, t)
+            var signed = lightwallet.signing.signTx(that.keystore, pwDerivedKey, t.serialize().toString('hex'), tx.from)
+            cb(null, '0x' + signed)
+          },
+          approveTransaction: function (tx, cb) {
+            console.log('approve', tx)
+            cb(null, true)
+          },
+          rpcUrl: 'https://kovan.infura.io'
+        }
+        that.providerOpts = opts
+        that.w3 = new Web3(new ZeroClientProvider(opts))
+        that.w3.eth.contract(abi).at(that.token.address, function (err, contract) {
+          if (err) throw err
+          that.token.contract = contract
+        })
       })
     },
     loadIFrame: function () {
-      this.$refs.appframe.onload = function () { console.log('loaded iframe', this) }
-      console.log(this.$refs.appframe.readyState)
-      this.$refs.appframe.src = this.iframe
       window.web3 = new Web3(new ZeroClientProvider(this.providerOpts))
-    },
-    setProvider: function () {
-      // noop for now until we figure out the exact flow.
+      this.$refs.appframe.src = this.iframe
     },
     savePassword: function () {
       if (this.password.length < 3) {
@@ -181,7 +230,8 @@ export default {
       status: 'Waiting…',
       iname: 'static/aepp',
       keystore: {},
-      token: {address: '0x35d8830ea35e6Df033eEdb6d5045334A4e34f9f9'},
+      token: {address: '0x35d8830ea35e6Df033eEdb6d5045334A4e34f9f9', decimals: new BigNumber(10).pow(18)},
+      tokenTransferValue: 0.0,
       providerOpts: undefined,
       addrList: [],
       tokens: [],
