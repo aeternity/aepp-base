feffcf873b747a50fe1c98f74d92920b85842747
Ali Baykal <abaykal@tutanota.com>
2017-10-27 18:48:41 +0200

    Animate id manager open and close

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,10 @@
 <template>
   <div id="app">
-    <id-manager v-if="showIdManager" :title="'Quick Id Manager'"></id-manager>
+    <id-manager :title="'Quick Id Manager'" :class="idManagerClassObject"></id-manager>
     <router-view></router-view>
   </div>
 </template>
 
 <script src='./App.js'/>
-<style src='./App.css'/>
+<style scoped src='./App.css'/>
+<style src='./App.global.css'/>
