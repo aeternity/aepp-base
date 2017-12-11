8b867df02806066b4844dddde4c0664098113e0a
Billy Rennekamp <billy.rennekamp@gmail.com>
2017-12-11 01:39:03 +0100

    functional but not persistent

--- src/components/Network.vue
+++ src/components/Network.vue
@@ -5,14 +5,16 @@
       name="example"
       :choices="options"
       :default="0"
+      @input="update"
     /></ae-switch>
-    <ae-button @click="addCustom = !addCustom"  :type="!addCustom ? 'exciting' : 'normal'">Add Custom Network</ae-button>
+   <!--  <ae-button @click="addCustom = !addCustom"  :type="!addCustom ? 'exciting' : 'normal'">Add Custom Network</ae-button>
     <br>
-    <ae-amount-input></ae-amount-input>
-    <input v-if="addCustom" v-model="custom" placeholder="Custom Network" ref="input">
+    <form @submit.prevent="addOption()">
+      <input v-if="addCustom" v-model="custom" placeholder="Custom Network" ref="input">
+    </form> -->
   </div>
 </template>
-</template>
+
 
 <script>
 import {
@@ -20,7 +22,7 @@ import {
   AeButton,
   AeAmountInput
 } from '@aeternity/aepp-components'
-import {mapActions} from '@/store'
+
 export default {
 
   name: 'Network',
@@ -29,26 +31,46 @@ export default {
     return {
       addCustom: false,
       custom: '',
+      current: 0,
       options: [
         { label: 'Kovan', value: 0, url: 'https://kovan.infura.io' },
         { label: 'Rinkeby', value: 1, url: 'https://rinkeby.infura.io' },
         { label: 'Ropsten', value: 2, url: 'https://ropsten.infura.io' },
-        { label: 'Localhost', value: 3, url: 'http://localhost:8545' }
+        { label: 'Localhost', value: 3, url: 'http://localhost:8545' },
+        { label: 'Mainnet', value: 4, url: 'https://mainnet.infura.io' }
       ]
     }
   },
+  watch: {
+    current () {
+      let option = this.options[this.current]
+      this.$store.dispatch('updateRPC', option.url)
+    }
+  },
   methods: {
-    addOptions (url) {
-      options.push({label: url, value: this.options.length, url})
+    addOption () {
+      this.options.push({label: this.custom, value: this.options.length, url: this.custom})
+    },
+    update (newVal) {
+      console.log(newVal)
+      this.current = newVal
     }
   },
   components: {
     AeSwitch,
     AeButton,
     AeAmountInput
+  },
+  mounted () {
+    console.log(this.$store)
   }
 }
 </script>
 
 <style lang="css" scoped>
+.network button, .network input, .network h1 {
+  margin:20px auto;
+  display: block;
+  text-align: center;
+}
 </style>
