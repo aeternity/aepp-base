d4a7af0699ce2b83ddf1ee862551f9c7d32ce62f
Denis Davidyuk <Denis_Davidyuk@hotmail.com>
2018-02-26 15:14:16 +0100

    AppBrowser: Update path according to `urlChanged` post message

--- src/pages/AppBrowser/AppBrowser.js
+++ src/pages/AppBrowser/AppBrowser.js
@@ -4,18 +4,26 @@ export default {
   name: 'app-browser',
   data () {
     return {
-      loading: true
+      loading: true,
+      path: `//${this.$route.fullPath}`
     }
   },
   components: { QuickId },
-  computed: {
-    path () {
-      return `//${this.$route.fullPath}`
+  methods: {
+    updateUrl ({ data: { method, payload: url } }) {
+      if (method !== 'urlChanged') return
+      this.$router.replace(url.replace(/^https?:\//i, ''))
     }
+  },
+  beforeMount () {
+    window.addEventListener('message', this.updateUrl, false)
   },
   mounted () {
     this.$refs.appframe.onload = () => {
       this.loading = false
     }
+  },
+  beforeDestroy () {
+    window.removeEventListener('message', this.updateUrl, false)
   }
 }
