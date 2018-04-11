676b3a864ecc5f63fc3d7ab167e141c3da24d39d
Philipp Klein <klein.philipp@gmail.com>
2018-04-11 18:15:34 +0200

    better interaface for network selector with add functionality and local storage

--- src/pages/Network.vue
+++ src/pages/Network.vue
@@ -1,21 +1,55 @@
 <template>
   <div class="network screen">
     <h1>Network</h1>
-    <ae-switch
-      name="example"
+    <ae-panel
       v-if="ready"
-      :choices="options"
-      :default="current"
-      @input="update"
-    />
-    <div class="status">
-     <b>Network id:</b> {{nodeSettings.host}}<br />
-    </div>
-   <!--  <ae-button @click="addCustom = !addCustom"  :type="!addCustom ? 'exciting' : 'normal'">Add Custom Network</ae-button>
-    <br>
-    <form @submit.prevent="addOption()">
-      <input v-if="addCustom" v-model="custom" placeholder="Custom Network" ref="input">
-    </form> -->
+      v-for='o, i in options'
+    >
+      <header>
+        <h2>{{ o.label }}</h2>
+      </header>
+      <code>
+        <span v-if='o.nodeSettings.secured'>https</span><span v-else>http</span>://{{o.nodeSettings.host}}:{{o.nodeSettings.port}}
+      </code>
+      <div class="grid">
+        <ae-button :inactive='nodeSettings.host === o.nodeSettings.host' @click="removeNetwork(i)" type='dramatic'>
+          <ae-icon slot='icon' invert type='dramatic' name='close' />
+          remove
+        </ae-button>
+        <ae-button :inactive='nodeSettings.host === o.nodeSettings.host' @click="updateNetwork(o)" type='exciting'>
+          <ae-icon slot='icon' invert type='exciting' name='check' />
+          activate
+        </ae-button>
+      </div>
+    </ae-panel>
+    <fixed-add-button @click='showAddOverlay = true'/>
+
+    <ae-modal v-if='showAddOverlay' title="Add Network">
+      <ae-label>Label</ae-label>
+      <ae-input v-model="newOptionToAdd.label"/>
+      <ae-label>Secure</ae-label>
+      <label class="container">Secure
+        <input type="radio" v-model="newOptionToAdd.secured" :value="true">
+        <span class="checkmark"></span>
+      </label>
+      <label class="container">
+        Non Secure
+        <input type="radio" v-model="newOptionToAdd.secured" :value="false">
+        <span class="checkmark"></span>
+      </label>
+      <ae-label>Host</ae-label>
+      <ae-input placeholder='example.com' v-model="newOptionToAdd.host"/>
+      <ae-label>Port</ae-label>
+      <ae-input v-model="newOptionToAdd.port"/>
+      <div class="grid">
+        <ae-button @click='showAddOverlay = false' type='boring'>
+          Cancel
+        </ae-button>
+        <ae-button @click='addNetwork' type='dramatic'>
+          Add
+        </ae-button>
+      </div>
+    </ae-modal>
     <quick-id showBackButton/>
   </div>
 </template>
@@ -26,9 +60,16 @@ import { mapState } from 'vuex'
 import {
   AeSwitch,
   AeButton,
+  AeInput,
+  AePanel,
+  AeLabel,
+  AeModal,
+  AeIcon,
   AeAmountInput
 } from '@aeternity/aepp-components'
 import QuickId from '@/components/QuickId.vue'
+import FixedAddButton from '@/components/FixedAddButton.vue'
+import ModalPage from '@/components/ModalPage.vue'
 
 export default {
   name: 'network',
@@ -38,6 +79,14 @@ export default {
       custom: '',
       current: 0,
       ready: false,
+      showAddOverlay: false,
+      newOptionToAdd: {
+
+        label: 'New Network',
+        host: '',
+        port: 443,
+        secured: true
+      },
       options: [
         {
           label: 'Devnet',
@@ -66,17 +115,44 @@ export default {
     ])
   },
   methods: {
-    update (newVal) {
-      if (this.current === newVal) return
-      this.current = newVal
-      let option = this.options[this.current]
-      console.log('should update to', option)
-      this.$store.commit('setNodeSettings', option.nodeSettings)
+    updateNetwork (newVal) {
+      this.$store.commit('setNodeSettings', newVal.nodeSettings)
+    },
+    removeNetwork (index) {
+      this.options.splice(index, 1)
+      this.saveNetworks()
+      console.log(index)
+    },
+    saveNetworks () {
+      localStorage.setItem(
+        'networks',
+        JSON.stringify(this.options)
+      )
+    },
+    addNetwork () {
+      this.options.push({
+        label: this.newOptionToAdd.label,
+        value: this.options.length,
+        nodeSettings: {
+          host: this.newOptionToAdd.host,
+          port: this.newOptionToAdd.port,
+          secured: this.newOptionToAdd.secured
+        }
+      })
+      this.saveNetworks()
+      this.showAddOverlay = false
     }
   },
   components: {
     AeSwitch,
     AeButton,
+    AePanel,
+    AeIcon,
+    AeInput,
+    AeLabel,
+    AeModal,
+    FixedAddButton,
+    ModalPage,
     AeAmountInput,
     QuickId
   },
@@ -87,6 +163,13 @@ export default {
     if (key > -1) {
       this.current = key
     }
+    try {
+    let options = JSON.parse(localStorage.getItem('networks'))
+    if (options.length > 0)
+      this.options = options
+    } catch (e) {
+      /* handle error */
+    }
     this.ready = true
   },
   destroyed () {
@@ -95,7 +178,7 @@ export default {
 }
 </script>
 
-<style lang="css" scoped>
+<style lang="scss" scoped>
 .network button, .network input, .network h1 {
   margin:20px auto;
   display: block;
@@ -104,5 +187,50 @@ export default {
 .network .status {
   margin: 20px auto;
   max-width: 600px;
+}
+.grid {
+  display:flex;
+}
+/* The container */
+.container {
+  display: block;
+  position: relative;
+  padding-left: 35px;
+  margin-bottom: 12px;
+  cursor: pointer;
+  font-size: 22px;
+  -webkit-user-select: none;
+  -moz-user-select: none;
+  -ms-user-select: none;
+  user-select: none;
+}
+
+/* Hide the browser's default radio button */
+.container input[type=radio] {
+  position: absolute;
+  opacity: 0;
+  cursor: pointer;
+}
+
+/* Create a custom radio button */
+.container .checkmark {
+  position: absolute;
+  top: 0;
+  left: 0;
+  height: 25px;
+  width: 25px;
+  background-color: #eee;
+  border-radius: 50%;
+}
+
+/* On mouse-over, add a grey background color */
+.container:hover input[type=radio] ~ .checkmark {
+  background-color: #ccc;
+}
+
+/* When the radio button is checked, add a blue background */
+.container input[type=radio]:checked ~ .checkmark {
+  $aubergine: #311b58;
+  background-color: $aubergine;
 }
 </style>
