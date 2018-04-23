bf6a06a0e2d38cea68971a7ff45ef35f903260b7
Denis Davidyuk <Denis_Davidyuk@hotmail.com>
2018-04-23 08:18:44 +0200

    AppBrowser: Refactor styles

--- src/pages/AppBrowser/AppBrowser.vue
+++ src/pages/AppBrowser/AppBrowser.vue
@@ -1,8 +1,8 @@
 <template>
-  <div class="app-browser screen" :class="{ loading }">
-    <iframe ref="appframe" id="appframe" class="nomargin" :src="path" />
+  <div class="app-browser screen">
+    <iframe ref="appframe" :class="{ loading }" class="nomargin" :src="path" />
 
-    <div class="loader" />
+    <div v-if="loading" class="loader" />
 
     <quick-id showBackButton />
   </div>
