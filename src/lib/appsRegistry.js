ba17989b37a882318db25b47cc9d93ddd99a1e03
Denis Davidyuk <Denis_Davidyuk@hotmail.com>
2018-06-07 08:33:15 +0200

    Remove incompatible apps

--- src/lib/appsRegistry.js
+++ src/lib/appsRegistry.js
@@ -1,21 +1,6 @@
 export const DEFAULT_ICON = 'static/icons/aepps/default.svg'
 
 export const appsRegistry = {
-  1: {
-    name: 'Proof',
-    icon: 'static/icons/aepps/proof.svg',
-    path: `${process.env.IS_STAGE ? 'stage-' : ''}proof.aepps.com`
-  },
-  2: {
-    name: 'Transfer',
-    icon: 'static/icons/aepps/transfer.svg',
-    path: 'transfer'
-  },
-  3: {
-    name: 'Wall',
-    icon: 'static/icons/aepps/wall.svg',
-    path: 'wall.aepps.com'
-  },
   4: {
     name: 'Settings',
     icon: 'static/icons/aepps/settings.svg',
