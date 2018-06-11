ccf1ef97bb752a1678f949dce06dd18de5d29865
Lukas Schönbächler <schoenbaechler.lukas@gmail.com>
2018-06-11 16:28:07 +0200

    changes to work with cordova

--- config/index.js
+++ config/index.js
@@ -45,12 +45,12 @@ module.exports = {
 
   build: {
     // Template for index.html
-    index: path.resolve(__dirname, '../dist/index.html'),
+    index: path.resolve(__dirname, '../www/index.html'),
 
     // Paths
-    assetsRoot: path.resolve(__dirname, '../dist'),
+    assetsRoot: path.resolve(__dirname, '../www'),
     assetsSubDirectory: 'static',
-    assetsPublicPath: '/',
+    assetsPublicPath: '',
 
     /**
      * Source Maps
