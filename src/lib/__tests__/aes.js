31275d182ed69fece614968c8d7ec4d1efcdd5f6
Denis Davidyuk <Denis_Davidyuk@hotmail.com>
2018-06-11 06:35:05 +0200

    aes.js: Add tests

---
+++ src/lib/__tests__/aes.js
@@ -0,0 +1,27 @@
+import AES from '../aes'
+
+if (window.crypto && window.crypto.subtle) {
+  throw new Error('This tests should be rewritten using provided Web Crypto API')
+}
+
+const genCryptTest = methodName => {
+  const getTestArray = length =>
+    new Uint8Array(new Array(length).fill().map((_, idx) => idx))
+  const testKey = getTestArray(16)
+  const testData = getTestArray(24)
+  const testResult = getTestArray(24).reverse
+  const cryptMethod = jest.fn().mockImplementation(() =>
+    new Promise(resolve => resolve(testResult)))
+  window.crypto = { subtle: { [methodName]: cryptMethod } }
+
+  const aes = new AES(testKey)
+  expect(aes[methodName](testData)).resolves.toBe(testResult)
+  expect(cryptMethod).toHaveBeenCalledWith(
+    { name: 'AES-CTR', counter: new Uint8Array(16), length: 128 },
+    testKey,
+    testData
+  )
+}
+
+it('encrypts', genCryptTest('encrypt'))
+it('decrypts', genCryptTest('decrypt'))
