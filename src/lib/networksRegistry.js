5801fce0baca81303ea455d2b9f76da1847b9834
Denis Davidyuk <Denis_Davidyuk@hotmail.com>
2018-06-12 12:16:04 +0200

    Change default network

--- src/lib/networksRegistry.js
+++ src/lib/networksRegistry.js
@@ -1,5 +1,5 @@
 export default [
+  { name: 'Testnet', url: 'https://test-net-api.aepps.com:443/api' },
   { name: 'localhost:3013', url: 'http://localhost:3013' },
-  { name: 'Testnet', url: 'https://test-net-api.aepps.com/api:443' },
   { name: 'SDK testnet', url: 'https://sdk-testnet.aepps.com:443' }
 ]
