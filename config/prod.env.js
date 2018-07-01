33be016264a633cb583f29086ab4c6be221129ff
Denis Davidyuk <Denis_Davidyuk@hotmail.com>
2018-07-01 14:07:51 +0200

    Add TARGET_DEVICE environment key for build process

--- config/prod.env.js
+++ config/prod.env.js
@@ -1,6 +1,14 @@
 'use strict'
+
+const { TARGET_DEVICE } = process.env
+const IS_CORDOVA = process.env.IS_CORDOVA === 'true'
+
 module.exports = {
   NODE_ENV: '"production"',
   IS_STAGE: process.env.TRAVIS_BRANCH === 'stage',
-  IS_CORDOVA: process.env.IS_CORDOVA === 'true'
+  IS_CORDOVA,
+  TARGET_DEVICE:
+    TARGET_DEVICE && JSON.stringify(TARGET_DEVICE) ||
+    IS_CORDOVA && '"mobile"' ||
+    undefined
 }
