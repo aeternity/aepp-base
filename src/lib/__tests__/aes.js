fbbe5736f3a39e7352f9918e01f934bcc76bc04f
Denis Davidyuk <Denis_Davidyuk@hotmail.com>
2018-07-25 19:04:21 +0200

    Simplify aes tests

--- src/lib/__tests__/aes.js
+++ src/lib/__tests__/aes.js
@@ -4,9 +4,10 @@ if (window.crypto && window.crypto.subtle) {
   throw new Error('This tests should be rewritten using provided Web Crypto API')
 }
 
+const getTestArray = length =>
+  new Uint8Array(length).map((_, idx) => idx)
+
 const genCryptTest = methodName => () => {
-  const getTestArray = length =>
-    new Uint8Array(new Array(length).fill().map((_, idx) => idx))
   const testKey = getTestArray(16)
   const testData = getTestArray(24)
   const testResult = getTestArray(24).reverse
