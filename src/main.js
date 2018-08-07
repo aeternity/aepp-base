10ef343cb51fefa9476fe53e8b924e37af2f0af4
Denis Davidyuk <Denis_Davidyuk@hotmail.com>
2018-08-07 13:21:33 +0200

    Remove outdated comment

--- src/main.js
+++ src/main.js
@@ -1,5 +1,3 @@
-// The Vue build version to load with the `import` command
-// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
 import Router from 'vue-router'
 import VeeValidate, { Validator } from 'vee-validate'
