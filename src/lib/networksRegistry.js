fb5a93b478ccccaec1794a1774b1e729842856ae
Denis Davidyuk <Denis_Davidyuk@hotmail.com>
2018-10-08 08:38:16 +0200

    Update @aeternity/aepp-sdk to 0.22.0-0.1.0-beta.1

--- src/lib/networksRegistry.js
+++ src/lib/networksRegistry.js
@@ -1,5 +1,6 @@
 export default [
   { name: 'SDK testnet', url: 'https://sdk-testnet.aepps.com:443' },
+  { name: 'SDK edgenet', url: 'https://sdk-edgenet.aepps.com' },
   { name: 'Testnet', url: 'https://test-net-api.aepps.com:443/api/' },
   { name: 'localhost:3013', url: 'http://localhost:3013' },
 ];
