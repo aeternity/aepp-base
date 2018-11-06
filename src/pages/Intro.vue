116bec0e19677fe7168c4adc6274d2e177949f24
Dmitry Kostin <etharner@gmail.com>
2018-11-06 20:35:15 +1000

    Intro.vue: Update design

--- src/pages/Intro.vue
+++ src/pages/Intro.vue
@@ -1,32 +1,38 @@
 <template>
   <mobile-page class="intro">
     <div class="logo">
-      <img src="../assets/intro.svg">
-      <p>
-        æternity gives you easy access to the magic of blockchain
-        technology. Instantly login or see how it works.
-      </p>
+      <guide
+        fill="neutral"
+      >
+        <em>Base æpp.</em>
+        <br>Interact with the
+        <br>æternity blockchain.
+        <br>In a human way.
+      </guide>
     </div>
 
     <template slot="footer">
       <ae-button
         :to="{ name: 'new-account' }"
-        type="exciting"
+        size="medium"
+        fill="secondary"
       >
-        Create Account
+        Create New Account
       </ae-button>
       <ae-button
         :to="{ name: keystore ? 'login' : 'recover' }"
-        size="small"
-        type="dramatic"
+        size="medium"
+        fill="neutral"
         plain
         uppercase
       >
         {{ keystore ? 'Login' : 'Recover' }}
       </ae-button>
+      <hr>
       <ae-button
         :to="{ name: 'onboarding' }"
-        size="small"
+        size="medium"
+        fill="neutral"
         plain
         uppercase>
         See how it works
@@ -37,11 +43,12 @@
 
 <script>
 import { mapState } from 'vuex';
-import { AeButton } from '@aeternity/aepp-components';
 import MobilePage from '../components/MobilePage.vue';
+import Guide from '../components/Guide.vue';
+import AeButton from '../components/AeButton.vue';
 
 export default {
-  components: { MobilePage, AeButton },
+  components: { MobilePage, AeButton, Guide },
   computed: mapState({
     keystore: state => state.mobile.keystore,
   }),
@@ -49,35 +56,41 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-@import '~@aeternity/aepp-components/dist/variables.scss';
+@import '~@aeternity/aepp-components-3/src/styles/placeholders/typography';
+@import '~@aeternity/aepp-components-3/src/styles/variables/colors';
 
 .intro.mobile-page {
-  background: transparent;
+  background: $color-primary;
 
   /deep/ .panel {
     background: transparent;
 
     .content {
-      flex-shrink: 0;
+      flex-grow: 0;
       display: flex;
     }
   }
 
   .logo {
-    text-align: center;
-    margin: auto;
+    margin-top: rem(10px);
 
-    img, p {
-      margin: 12px;
+  }
+
+  /deep/ .footer {
+    margin: 0 rem(32px);
+
+    .ae-button {
+      margin: 0 auto;
     }
 
-    p {
-      max-width: 300px;
-      font-size: 18px;
-      line-height: 1.56;
-      color: $anthracite;
+    hr {
+      margin: 0 rem(20px);
+      height: 2px;
+      border: none;
+      background-color: $color-primary-negative-1;
     }
   }
 }
 </style>
 <style lang="scss" src="../components/MobilePageContent.scss" scoped />
+<style lang="scss" src="./Intro.scss" scoped />
