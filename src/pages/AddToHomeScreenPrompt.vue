82d066e1ded13db9c7149d4934160087be59692f
Dmitry Kostin <etharner@gmail.com>
2018-11-10 04:38:42 +1000

    AddToHomeScreenPrompt.vue: Update design accordingly to other prompt

--- src/pages/AddToHomeScreenPrompt.vue
+++ src/pages/AddToHomeScreenPrompt.vue
@@ -7,18 +7,20 @@
 </template>
 
 <style lang="scss" scoped>
-@import '~@aeternity/aepp-components/dist/variables.scss';
+@import '~@aeternity/aepp-components-3/src/styles/variables/colors';
 
 .add-to-home-screen-prompt {
   flex-grow: 1;
   display: flex;
+  background-color: $color-primary;
 
   p {
     text-align: center;
     margin: auto;
     max-width: 300px;
-    font-size: 18px;
+    font-size: 24px;
     line-height: 1.56;
+    color: $color-neutral-maximum;
   }
 }
 </style>
