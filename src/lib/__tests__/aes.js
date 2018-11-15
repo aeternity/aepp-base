3949fc115500025b144ce95b9fc264e27dfeb95a
Denis Davidyuk <denis_davidyuk@hotmail.com>
2018-11-15 10:41:24 +0100

    Fix AES-CTR counter issue

--- src/lib/__tests__/aes.js
+++ src/lib/__tests__/aes.js
@@ -5,24 +5,56 @@ if (window.crypto && window.crypto.subtle) {
 }
 
 const getTestArray = length =>
-  new Uint8Array(length).map((_, idx) => idx);
+  Buffer.alloc(length).map((_, idx) => idx);
+
+const testKey = getTestArray(16);
+const testData = getTestArray(24);
+const testResult = getTestArray(24).reverse();
+
+const mockCryptMethod = (methodName) => {
+  const cryptMethod = jest.fn().mockImplementation(() => Promise.resolve(testResult));
+  window.crypto = { subtle: { [methodName]: cryptMethod } };
+};
+
+const unMockCryptMethod = () => {
+  delete window.crypto;
+};
 
 const genCryptTest = methodName => () => {
-  const testKey = getTestArray(16);
-  const testData = getTestArray(24);
-  const testResult = getTestArray(24).reverse;
-  const cryptMethod = jest.fn().mockImplementation(() =>
-    new Promise(resolve => resolve(testResult)));
-  window.crypto = { subtle: { [methodName]: cryptMethod } };
-
+  mockCryptMethod(methodName);
   const aes = new AES(testKey);
   expect(aes[methodName](testData)).resolves.toBe(testResult);
-  expect(cryptMethod).toHaveBeenCalledWith(
-    { name: 'AES-CTR', counter: new Uint8Array(16), length: 128 },
+  expect(window.crypto.subtle[methodName]).toHaveBeenCalledWith(
+    { name: 'AES-CTR', counter: Buffer.alloc(16), length: 128 },
     testKey,
     testData,
   );
+  unMockCryptMethod();
 };
 
-it('encrypts', genCryptTest('encrypt'));
-it('decrypts', genCryptTest('decrypt'));
+const genCryptCounterTest = methodName => async () => {
+  mockCryptMethod(methodName);
+  const aes = new AES(testKey);
+  await aes[methodName](testData);
+  aes[methodName](testData);
+  const counter = Buffer.alloc(16);
+  counter[15] = 2;
+  expect(window.crypto.subtle[methodName]).toHaveBeenLastCalledWith(
+    { name: 'AES-CTR', counter, length: 128 },
+    testKey,
+    testData,
+  );
+  unMockCryptMethod();
+};
+
+describe('encrypt', () => {
+  it('encrypts', genCryptTest('encrypt'));
+
+  it('increases counter value', genCryptCounterTest('encrypt'));
+});
+
+describe('decrypt', () => {
+  it('decrypts', genCryptTest('decrypt'));
+
+  it('increases counter value', genCryptCounterTest('decrypt'));
+});
