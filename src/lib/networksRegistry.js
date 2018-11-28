846fbaacf86a194c5d2312440ed95a19c5d22fe1
Denis Davidyuk <denis_davidyuk@hotmail.com>
2018-11-28 14:00:38 +0100

    networkRegistry: Remove default https port

--- src/lib/networksRegistry.js
+++ src/lib/networksRegistry.js
@@ -1,6 +1,6 @@
 export default [
-  { name: 'SDK testnet', url: 'https://sdk-testnet.aepps.com:443' },
+  { name: 'SDK testnet', url: 'https://sdk-testnet.aepps.com' },
   { name: 'SDK edgenet', url: 'https://sdk-edgenet.aepps.com' },
-  { name: 'Testnet', url: 'https://test-net-api.aepps.com:443/api/' },
+  { name: 'Testnet', url: 'https://test-net-api.aepps.com/api/' },
   { name: 'localhost:3013', url: 'http://localhost:3013' },
 ];
