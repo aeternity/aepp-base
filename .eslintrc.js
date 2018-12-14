ad24828b9afb930778c121b6cab93f79edca0c79
Denis Davidyuk <denis_davidyuk@hotmail.com>
2018-12-14 04:36:59 +0100

    Preserve kebab-base from component names for now

--- .eslintrc.js
+++ .eslintrc.js
@@ -10,6 +10,7 @@ module.exports = {
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'vue/component-name-in-template-casing': 'kebab-case',
   },
   parserOptions: {
     parser: 'babel-eslint',
