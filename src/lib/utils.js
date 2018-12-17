13f938cc87e096c8298f5e580fd9fd6a0bf6bd31
Denis Davidyuk <denis_davidyuk@hotmail.com>
2018-12-17 14:54:00 +0100

    Fix toUrl helping function

--- src/lib/utils.js
+++ src/lib/utils.js
@@ -1,2 +1,2 @@
 // eslint-disable-next-line import/prefer-default-export
-export const toUrl = url => new URL((/^\w+:\//.test(this.url) ? '' : 'http://') + url);
+export const toUrl = url => new URL((/^\w+:\//.test(url) ? '' : 'http://') + url);
