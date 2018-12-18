8840317e882d8de205059e45604a6d4eb47c1f96
Denis Davidyuk <denis_davidyuk@hotmail.com>
2018-12-18 09:19:27 +0100

    AeAddressInput: Fix tests

--- src/components/__tests__/AeAddressInput.js
+++ src/components/__tests__/AeAddressInput.js
@@ -7,37 +7,40 @@ const testAddressFormatted = 'ak_ 123 456 789 123 456 789 123 456 789 123 456 78
 describe('AeAddressInput', () => {
   it('formats passed value', () => {
     const wrapper = mount(AeAddressInput, { propsData: { value: testAddress } });
-    expect(wrapper.element.value).toBe(testAddressFormatted);
+    expect(wrapper.find('textarea').element.value).toBe(testAddressFormatted);
   });
 
   it('adds prefix on input', () => {
     const wrapper = mount(AeAddressInput);
-    wrapper.element.value = 'beef';
-    wrapper.trigger('input');
+    const textarea = wrapper.find('textarea');
+    textarea.element.value = 'beef';
+    textarea.trigger('input');
 
     const emittedValue = wrapper.emitted('input')[0][0];
     expect(emittedValue).toEqual('ak_beef');
     wrapper.setProps({ value: emittedValue });
-    expect(wrapper.element.value).toBe('ak_ bee f');
+    expect(textarea.element.value).toBe('ak_ bee f');
   });
 
   it('removes non-base58 symbols', () => {
     const wrapper = mount(AeAddressInput);
     const value = 'ak_019AHIJNOPZaklmz';
-    wrapper.element.value = value;
-    wrapper.element.setSelectionRange(value.length, value.length);
-    wrapper.trigger('input');
+    const textarea = wrapper.find('textarea');
+    textarea.element.value = value;
+    textarea.element.setSelectionRange(value.length, value.length);
+    textarea.trigger('input');
 
     const emittedValue = wrapper.emitted('input')[0][0];
     expect(emittedValue).toEqual('ak_19AHJNPZakmz');
     wrapper.setProps({ value: emittedValue });
-    expect(wrapper.element.value).toBe('ak_ 19A HJN PZa kmz');
+    expect(textarea.element.value).toBe('ak_ 19A HJN PZa kmz');
   });
 
   it('emitted input event contains address without space symbols', () => {
     const wrapper = mount(AeAddressInput, { propsData: { value: testAddress } });
-    wrapper.element.setSelectionRange(10, 10);
-    wrapper.trigger('input');
+    const textarea = wrapper.find('textarea');
+    textarea.element.setSelectionRange(10, 10);
+    textarea.trigger('input');
     expect(wrapper.emitted().input[0][0]).toBe(testAddress);
   });
 
@@ -45,26 +48,28 @@ describe('AeAddressInput', () => {
     const wrapper = mount(AeAddressInput);
     const begin = testAddressFormatted.slice(0, 25);
     const end = `${testAddressFormatted.slice(25)}test`;
-    wrapper.element.value = begin + end;
-    wrapper.element.setSelectionRange(begin.length, begin.length);
-    wrapper.trigger('input');
+    const textarea = wrapper.find('textarea');
+    textarea.element.value = begin + end;
+    textarea.element.setSelectionRange(begin.length, begin.length);
+    textarea.trigger('input');
 
     const emittedValue = wrapper.emitted('input')[0][0];
     expect(emittedValue).toEqual(testAddress);
     wrapper.setProps({ value: emittedValue });
-    expect(wrapper.element.value).toBe(testAddressFormatted);
+    expect(textarea.element.value).toBe(testAddressFormatted);
   });
 
   it('backspace can remove the whole value', () => {
     const wrapper = mount(AeAddressInput, { propsData: { value: 'ak_12' } });
 
-    while (wrapper.element.value) {
-      const { value } = wrapper.element;
-      wrapper.element.value = value.slice(0, value.length - 1);
-      wrapper.element.setSelectionRange(value.length - 1, value.length - 1);
-      wrapper.trigger('input');
+    const textarea = wrapper.find('textarea');
+    while (textarea.element.value) {
+      const { value } = textarea.element;
+      textarea.element.value = value.slice(0, value.length - 1);
+      textarea.element.setSelectionRange(value.length - 1, value.length - 1);
+      textarea.trigger('input');
       wrapper.setProps({ value: wrapper.emitted('input').pop()[0] });
-      expect(wrapper.element.value.length).toBeLessThan(value.length);
+      expect(textarea.element.value.length).toBeLessThan(value.length);
     }
   });
 });
