53d4b064e991a7560e170b26e939f5d52e5b9fe6
Denis Davidyuk <denis_davidyuk@hotmail.com>
2018-12-21 08:34:29 +0100

    Remove outdated network

--- src/lib/networksRegistry.js
+++ src/lib/networksRegistry.js
@@ -2,6 +2,5 @@ export default [
   { name: 'SDK testnet', url: 'https://sdk-testnet.aepps.com' },
   { name: 'SDK edgenet', url: 'https://sdk-edgenet.aepps.com' },
   { name: 'SDK mainnet', url: 'https://sdk-mainnet.aepps.com' },
-  { name: 'Testnet', url: 'https://test-net-api.aepps.com/api/' },
   { name: 'localhost:3013', url: 'http://localhost:3013' },
 ];
