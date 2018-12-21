84025201fddb7ac973763e4dafcd16ea205b8d68
Denis Davidyuk <denis_davidyuk@hotmail.com>
2018-12-21 08:35:19 +0100

    Remove localhost:3013 network as far as it can be added manually

--- src/lib/networksRegistry.js
+++ src/lib/networksRegistry.js
@@ -2,5 +2,4 @@ export default [
   { name: 'SDK testnet', url: 'https://sdk-testnet.aepps.com' },
   { name: 'SDK edgenet', url: 'https://sdk-edgenet.aepps.com' },
   { name: 'SDK mainnet', url: 'https://sdk-mainnet.aepps.com' },
-  { name: 'localhost:3013', url: 'http://localhost:3013' },
 ];
