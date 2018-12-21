443432c6ae0cb6d8578b8d61dce4522190f5dd9e
Denis Davidyuk <denis_davidyuk@hotmail.com>
2018-12-21 18:12:34 +0100

    Use mainnet by default

--- src/lib/networksRegistry.js
+++ src/lib/networksRegistry.js
@@ -1,5 +1,5 @@
 export default [
+  { name: 'SDK mainnet', url: 'https://sdk-mainnet.aepps.com' },
   { name: 'SDK testnet', url: 'https://sdk-testnet.aepps.com' },
   { name: 'SDK edgenet', url: 'https://sdk-edgenet.aepps.com' },
-  { name: 'SDK mainnet', url: 'https://sdk-mainnet.aepps.com' },
 ];
