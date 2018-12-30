a72fe1954317642c6a61920aebcbc4248e9bfdbf
Denis Davidyuk <denis_davidyuk@hotmail.com>
2018-12-30 17:23:50 +0100

    storybook: Add notes addon

--- config/storybook/config.js
+++ config/storybook/config.js
@@ -1,8 +1,10 @@
 /* eslint-disable import/no-extraneous-dependencies */
 import { configure, addDecorator } from '@storybook/vue';
 import { withBackgrounds } from '@storybook/addon-backgrounds';
+import { withNotes } from '@storybook/addon-notes';
 
 addDecorator(withBackgrounds);
+addDecorator(withNotes);
 
 const req = require.context('../../src/stories', true, /.stories.js$/);
 
