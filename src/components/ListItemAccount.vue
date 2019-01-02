58b6c61d677abf6b6c9ce31763269250d369f8a3
Denis Davidyuk <denis_davidyuk@hotmail.com>
2019-01-02 08:18:12 +0100

    Extract ListItemAccount component

---
+++ src/components/ListItemAccount.vue
@@ -0,0 +1,36 @@
+<template>
+  <list-item
+    v-bind="$attrs"
+    :title="name"
+    :subtitle="prefixedAmount(balance) + ' AE'"
+    subtitle-monospace
+    v-on="$listeners"
+  >
+    <ae-identicon
+      slot="icon"
+      :address="address"
+    />
+    <slot
+      v-for="slot in Object.keys($slots)"
+      :slot="slot"
+      :name="slot"
+    />
+  </list-item>
+</template>
+
+<script>
+import BigNumber from 'bignumber.js';
+import { AeIdenticon } from '@aeternity/aepp-components-3';
+import ListItem from './ListItem.vue';
+import prefixedAmount from '../filters/prefixedAmount';
+
+export default {
+  components: { ListItem, AeIdenticon },
+  props: {
+    name: { type: String, required: true },
+    address: { type: String, required: true },
+    balance: { type: BigNumber, required: true },
+  },
+  methods: { prefixedAmount },
+};
+</script>
