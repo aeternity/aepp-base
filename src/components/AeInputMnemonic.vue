0b27330a9ee0ab3601225204b2af595c7af46478
Denis Davidyuk <denis_davidyuk@hotmail.com>
2019-01-02 15:59:54 +0100

    Add AeInputMnemonic component

---
+++ src/components/AeInputMnemonic.vue
@@ -0,0 +1,29 @@
+<template>
+  <ae-textarea-formatted
+    class="ae-input-mnemonic"
+    header="Recovery phrase"
+    placeholder="Recovery phrase"
+    rows="3"
+    :format-display-value="formatMnemonic"
+    v-bind="$attrs"
+    :value="value"
+    v-on="$listeners"
+  />
+</template>
+
+<script>
+import AeTextareaFormatted from './AeTextareaFormatted.vue';
+
+export default {
+  components: { AeTextareaFormatted },
+  props: {
+    value: { type: String, required: true },
+  },
+  methods: {
+    formatMnemonic: address => address
+      .toLowerCase()
+      .replace(/\s+/g, ' ')
+      .replace(/[^a-z ]/g, ''),
+  },
+};
+</script>
