9816758bb7a3053666fd9ee7860eafeb832f6683
Denis Davidyuk <denis_davidyuk@hotmail.com>
2019-01-06 12:52:42 +0100

    TabBar: Limit stretching

--- src/components/mobile/TabBar.vue
+++ src/components/mobile/TabBar.vue
@@ -1,27 +1,29 @@
 <template>
   <div class="tab-bar">
-    <router-link :to="{ name: 'apps' }">
-      <ae-icon name="grid" />
-      Browser
-    </router-link>
-    <router-link :to="{ name: 'transfer' }">
-      <ae-icon name="transfer" />
-      Transfer
-    </router-link>
-    <button
-      :class="showAccountSwitcher ? 'router-link-active' : ''"
-      @click="() => !showAccountSwitcher && toggleAccountSwitcher()"
-    >
-      <ae-identity-avatar :address="activeIdentity.address" />
-    </button>
-    <router-link :to="{ name: 'address-book' }">
-      <ae-icon name="contacts" />
-      Contacts
-    </router-link>
-    <router-link :to="{ name: 'settings' }">
-      <ae-icon name="settings" />
-      Settings
-    </router-link>
+    <div class="wrapper">
+      <router-link :to="{ name: 'apps' }">
+        <ae-icon name="grid" />
+        Browser
+      </router-link>
+      <router-link :to="{ name: 'transfer' }">
+        <ae-icon name="transfer" />
+        Transfer
+      </router-link>
+      <button
+        :class="showAccountSwitcher ? 'router-link-active' : ''"
+        @click="() => !showAccountSwitcher && toggleAccountSwitcher()"
+      >
+        <ae-identity-avatar :address="activeIdentity.address" />
+      </button>
+      <router-link :to="{ name: 'address-book' }">
+        <ae-icon name="contacts" />
+        Contacts
+      </router-link>
+      <router-link :to="{ name: 'settings' }">
+        <ae-icon name="settings" />
+        Settings
+      </router-link>
+    </div>
   </div>
 </template>
 
@@ -44,55 +46,61 @@ export default {
 
 <style scoped lang="scss">
 @import '~@aeternity/aepp-components-3/src/styles/variables/typography.scss';
+@import '~@aeternity/aepp-components-3/src/styles/fallback/variables.scss';
 
 .tab-bar {
-  display: flex;
-  align-items: center;
   position: fixed;
   left: 0;
   right: 0;
   bottom: 0;
-  padding: 10px;
   background-color: #001833;
 
-  a, button {
-    flex-grow: 1;
-    flex-basis: 0;
-    font-family: $font-sans;
-    font-size: 11px;
-    font-weight: 500;
-    line-height: 1.45;
-    letter-spacing: 0.2px;
-    color: #76818c;
-    text-decoration: none;
-    text-align: center;
+  .wrapper {
+    display: flex;
+    align-items: center;
+    max-width: $screen-phone;
+    margin: 0 auto;
+    padding: 10px;
 
-    .ae-icon {
-      font-size: 20px;
-      display: block;
-      margin-bottom: 4px;
-    }
+    a, button {
+      flex-grow: 1;
+      flex-basis: 0;
+      font-family: $font-sans;
+      font-size: 11px;
+      font-weight: 500;
+      line-height: 1.45;
+      letter-spacing: 0.2px;
+      color: #76818c;
+      text-decoration: none;
+      text-align: center;
 
-    .ae-identity-avatar {
-      width: 42px;
-      height: 42px;
-      border: 2px solid #000;
-      box-shadow: 0 0 0 2px #ff0d6a;
-    }
-
-    &.router-link-active {
-      color: #fff;
+      .ae-icon {
+        font-size: 20px;
+        display: block;
+        margin-bottom: 4px;
+      }
 
       .ae-identity-avatar {
-        box-shadow: 0 0 0 2px #fff;
+        width: 42px;
+        height: 42px;
+        border: 2px solid #000;
+        box-shadow: 0 0 0 2px #ff0d6a;
+      }
+
+      &.router-link-active {
+        color: #fff;
+
+        .ae-identity-avatar {
+          box-shadow: 0 0 0 2px #fff;
+        }
       }
     }
-  }
 
-  button {
-    display: inline;
-    background-color: transparent;
-    border: none;
+    button {
+      display: inline;
+      background-color: transparent;
+      border: none;
+    }
   }
 }
 </style>
