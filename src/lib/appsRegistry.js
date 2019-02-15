6cf398695c162c334a6da6c6864dd8916f9ad6a5
Denis Davidyuk <denis_davidyuk@hotmail.com>
2019-02-15 10:32:34 +0100

    Remove unused apps stuff

--- src/lib/appsRegistry.js
+++ src/lib/appsRegistry.js
@@ -1,30 +1,7 @@
-import transferIcon from '../assets/icons/aepps/transfer.svg';
-import settingsIcon from '../assets/icons/aepps/settings.svg';
-import addressesIcon from '../assets/icons/aepps/addresses.svg';
 import blockchainExplorerIcon from '../assets/icons/aepps/blockchain-explorer.svg';
 import tokenMigrationIcon from '../assets/icons/aepps/token-migration.svg';
 
 export { default as DEFAULT_ICON } from '../assets/icons/aepps/blockchain-explorer.svg';
-
-export const appsRegistry = {
-  2: {
-    name: 'Transfer',
-    icon: transferIcon,
-    path: 'transfer',
-  },
-  4: {
-    name: 'Settings',
-    icon: settingsIcon,
-    path: 'settings',
-    unremovable: true,
-  },
-  5: {
-    name: 'Addresses',
-    icon: addressesIcon,
-    iconFullSize: true,
-    path: 'addresses',
-  },
-};
 
 export const aeternityApps = [{
   name: 'Blockchain Explorer',
--- src/router/routes/common.js
+++ src/router/routes/common.js
@@ -1,16 +1,10 @@
 import { merge } from 'lodash-es';
 import { checkLoggedIn } from '../utils';
-import AddApp from '../../pages/AddApp.vue';
 import AddressBook from '../../pages/AddressBook.vue';
 import AddressBookNew from '../../pages/AddressBookNew.vue';
 import AddressBookChoose from '../../pages/AddressBookChoose.vue';
 
 export default [{
-  name: 'add-app',
-  path: '/add-app',
-  component: AddApp,
-  beforeEnter: checkLoggedIn(true),
-}, {
   name: 'address-book',
   path: '/addresses',
   component: AddressBook,
