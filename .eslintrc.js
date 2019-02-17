c254e8013457c79739707a30ae9738bca82fd452
Denis Davidyuk <denis_davidyuk@hotmail.com>
2019-02-17 11:12:18 +0100

    Reset "vue/component-name-in-template-casing" rule to default

--- .eslintrc.js
+++ .eslintrc.js
@@ -10,7 +10,6 @@ module.exports = {
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-    'vue/component-name-in-template-casing': 'kebab-case',
   },
   parserOptions: {
     parser: 'babel-eslint',
