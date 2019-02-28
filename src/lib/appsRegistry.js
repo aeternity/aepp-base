0e3e709c2ed9c4fc3d83773837f5227385add650
Denis Davidyuk <denis_davidyuk@hotmail.com>
2019-02-28 11:38:28 +0100

    Add Example app to aeternityApps in not production

--- src/lib/appsRegistry.js
+++ src/lib/appsRegistry.js
@@ -13,4 +13,9 @@ export const aeternityApps = [{
   description: 'AE token migration to the æternity Mainnet is ongoing. If you still haven\'t transfered your Ethereum AE tokens, you can do it using this app.',
   path: 'token-migration.aepps.com',
   icon: tokenMigrationIcon,
-}];
+}, ...process.env.NODE_ENV === 'production' ? [] : [{
+  name: 'Example æpp',
+  description: 'Based on the example from JS SDK.',
+  path: 'example-aepp.origin.aepps.com',
+  icon: blockchainExplorerIcon,
+}]];
