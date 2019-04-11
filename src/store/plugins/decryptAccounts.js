96d38b2e85f89d3b38730ba37a47ae1fb524b75f
Denis Davidyuk <denis_davidyuk@hotmail.com>
2019-04-11 17:00:35 +0200

    Fix exception on Login/New Account/Recover

--- src/store/plugins/decryptAccounts.js
+++ src/store/plugins/decryptAccounts.js
@@ -4,7 +4,10 @@ import AES from '../../lib/aes';
 export default store => store.watch(
   ({ mobile: { keystore, accountCount, derivedKey } }) => [keystore, accountCount, derivedKey],
   async ([keystore, accountCount, derivedKey]) => {
-    if (!keystore || !derivedKey) return;
+    if (!keystore || !derivedKey) {
+      store.commit('setAccounts', []);
+      return;
+    }
     const aes = new AES(derivedKey);
     const wallet = {
       privateKey: await aes.decrypt(keystore.privateKey),
