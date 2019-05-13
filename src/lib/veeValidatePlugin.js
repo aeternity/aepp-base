63a098e51771a7618cdd2b660c6aecfcbe31ce2f
Denis Davidyuk <denis_davidyuk@hotmail.com>
2019-05-13 20:15:22 +0200

    Drop unused parts of vee-validate
    https://baianat.github.io/vee-validate/concepts/bundle-size.html

--- src/lib/veeValidatePlugin.js
+++ src/lib/veeValidatePlugin.js
@@ -1,12 +1,23 @@
-import VeeValidate, { Validator } from 'vee-validate';
+import { Validator, install as VeeValidate } from 'vee-validate/dist/vee-validate.minimal.esm';
+import {
+  confirmed, decimal, excluded, min, required,
+} from 'vee-validate/dist/rules.esm';
 import BigNumber from 'bignumber.js';
 import { Crypto } from '@aeternity/aepp-sdk/es';
 import { validateMnemonic } from '@aeternity/bip39';
 import { toUrl } from './utils';
 
-Validator.extend('min_value_exclusive', (value, [min]) => BigNumber(value).isGreaterThan(min));
-Validator.extend('min_value', (value, [max]) => BigNumber(value).isGreaterThanOrEqualTo(max));
-Validator.extend('max_value', (value, [max]) => BigNumber(value).isLessThanOrEqualTo(max));
+Validator.extend('confirmed', confirmed);
+Validator.extend('decimal', decimal);
+Validator.extend('excluded', excluded);
+Validator.extend('min', min);
+Validator.extend('required', required);
+
+Validator.extend('address', value => Crypto.isAddressValid(value));
+Validator.extend('max_value', (value, [arg]) => BigNumber(value).isLessThanOrEqualTo(arg));
+Validator.extend('min_value', (value, [arg]) => BigNumber(value).isGreaterThanOrEqualTo(arg));
+Validator.extend('min_value_exclusive', (value, [arg]) => BigNumber(value).isGreaterThan(arg));
+Validator.extend('mnemonic', value => validateMnemonic(value));
 Validator.extend('url_http', (value) => {
   try {
     const url = toUrl(value);
@@ -15,27 +26,24 @@ Validator.extend('url_http', (value) => {
     return false;
   }
 });
-Validator.extend('address', value => Crypto.isAddressValid(value));
-Validator.extend('mnemonic', value => validateMnemonic(value));
+
+Validator.localize('en', {
+  messages: {
+    confirmed: () => 'Passwords don\'t match, try again',
+    decimal: (field, [decimals = '*'] = []) => `This field must be numeric and may contain ${!decimals || decimals === '*' ? '' : decimals} decimal points`,
+    excluded: () => 'This field must be a valid value.',
+    min: (field, [length]) => `This field must be at least ${length} characters`,
+    required: 'This field is required',
+
+    address: () => 'Invalid AE Address',
+    max_value: (field, [arg]) => `This field must be ${arg} or less`,
+    min_value: (field, [arg]) => `This field must be ${arg} or more`,
+    min_value_exclusive: (field, [arg]) => `This field must be more than ${arg}`,
+    mnemonic: () => 'Invalid recovery phrase',
+    url_http: () => 'This field is not a valid HTTP(S) URL',
+  },
+});
 
 export default {
-  install: Vue => Vue.use(VeeValidate, {
-    dictionary: {
-      en: {
-        messages: {
-          required: 'This field is required',
-          min: (field, [length]) => `This field must be at least ${length} characters`,
-          min_value: (field, [min]) => `This field must be ${min} or more`,
-          min_value_exclusive: (field, [min]) => `This field must be more than ${min}`,
-          max_value: (field, [max]) => `This field must be ${max} or less`,
-          not_in: () => 'This field must be a valid value',
-          decimal: (field, [decimals = '*'] = []) => `This field must be numeric and may contain ${!decimals || decimals === '*' ? '' : decimals} decimal points`,
-          url_http: () => 'This field is not a valid HTTP(S) URL',
-          confirmed: () => 'Passwords don\'t match, try again',
-          address: () => 'Invalid AE Address',
-          mnemonic: () => 'Invalid recovery phrase',
-        },
-      },
-    },
-  }),
+  install: Vue => Vue.use(VeeValidate),
 };
