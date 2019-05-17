87f6d71428a7e9f847268a358e146dffd8a414ee
Denis Davidyuk <denis_davidyuk@hotmail.com>
2019-05-17 18:46:38 +0200

    Setup Countly analytics

--- src/ui.js
+++ src/ui.js
@@ -1,6 +1,8 @@
 import Vue from 'vue';
 import Router from 'vue-router';
 import Matomo from 'vue-matomo';
+import Countly from 'countly-sdk-web';
+import { defer } from 'lodash-es';
 import './ui-common';
 import './register-modals';
 import sync from './lib/vuexRouterSync';
@@ -18,6 +20,14 @@ Vue.use(Matomo, {
   siteId: process.env.VUE_APP_MATOMO_SITE_ID,
   router,
 });
+
+Countly.init({
+  url: process.env.VUE_APP_COUNTLY_URL,
+  app_key: process.env.VUE_APP_COUNTLY_APP_KEY,
+});
+Countly.q.push(['track_sessions']);
+router.afterEach(() => defer(() => Countly.q
+  .push(['track_pageview', window.location.pathname + window.location.hash])));
 
 sync(store, router);
 uiPlugin(store);
