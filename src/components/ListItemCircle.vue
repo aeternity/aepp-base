b377b7ea99a1752925c14e93d9eb0baf5e23ace4
Denis Davidyuk <denis_davidyuk@hotmail.com>
2019-05-24 06:27:18 +0200

    ListItemCircle: Set font-size: 0 if icon in slot

--- src/components/ListItemCircle.vue
+++ src/components/ListItemCircle.vue
@@ -1,8 +1,21 @@
 <template>
-  <div class="list-item-circle">
+  <div :class="['list-item-circle', hasIcon && 'has-icon']">
     <slot />
   </div>
 </template>
+
+<script>
+import { get } from 'lodash-es';
+
+export default {
+  computed: {
+    hasIcon() {
+      return get(this.$slots.default, '[0].tag', '') === 'svg'
+        && get(this.$slots.default, '[0].data.class', []).join(' ').includes('icon');
+    },
+  },
+};
+</script>
 
 <style lang="scss" scoped>
 @import '~@aeternity/aepp-components-3/src/styles/globals/functions.scss';
@@ -17,6 +30,10 @@
   box-shadow: 0 0 rem(16px) $color-shadow-alpha-15;
   text-align: center;
   color: #fff;
+
+  &.has-icon {
+    font-size: 0;
+  }
 
   .icon {
     height: rem(15px);
