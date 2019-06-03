7c55a74beac02d8503b4b4687bb7e7573df4d9de
Denis Davidyuk <denis_davidyuk@hotmail.com>
2019-06-03 10:38:49 +0200

    transaction list: Don't swallow middleware errors

--- src/store/plugins/ui/observables.js
+++ src/store/plugins/ui/observables.js
@@ -112,10 +112,8 @@ export default (store) => {
     mdwUrl.searchParams.set('limit', limit.toString());
     mdwUrl.searchParams.set('page', page.toString());
     const txs = await Promise.all(
-      mapKeysDeep(
-        await fetchJson(mdwUrl).catch(() => []),
-        (value, key) => camelCase(key),
-      ).map(normalizeTransaction),
+      mapKeysDeep(await fetchJson(mdwUrl), (value, key) => camelCase(key))
+        .map(normalizeTransaction),
     );
     txs.forEach(registerTx);
     return txs;
