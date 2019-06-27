1b75dd402422abd0066f2b4e5c695b1983ab747c
Denis Davidyuk <denis_davidyuk@hotmail.com>
2019-06-27 21:43:43 +0200

    Drop Countly

--- src/setupAnalytics.js
+++ src/setupAnalytics.js
@@ -1,5 +1,4 @@
 import Matomo from 'vue-matomo/src/matomo';
-import Countly from 'countly-sdk-web';
 import { defer } from 'lodash-es';
 import routerPromise from './router';
 import store from './store';
@@ -19,33 +18,14 @@ export default async () => {
   matomo.enableLinkTracking();
   matomo.enableJSErrorTracking();
 
-  Countly.init({
-    url: process.env.VUE_APP_COUNTLY_URL,
-    app_key: process.env.VUE_APP_COUNTLY_APP_KEY,
-    app_version: process.env.npm_package_version,
-  });
-  Countly.add_event({ key: `is-mobile-device:${process.env.IS_MOBILE_DEVICE}` });
-  Countly.add_event({ key: `is-pwa:${process.env.IS_PWA}` });
-  Countly.add_event({ key: `is-ios:${process.env.IS_IOS}` });
-  Countly.add_event({ key: `is-cordova:${process.env.IS_CORDOVA}` });
-  Countly.add_event({
-    key: 'accounts-count',
-    sum: store.state.accounts.list.length,
-  });
-  Countly.track_sessions();
-  Countly.track_links();
-  Countly.track_errors();
-
   (await routerPromise).afterEach(() => defer(() => {
     const url = window.location.pathname + window.location.hash;
     matomo.setCustomUrl(url);
     matomo.trackPageView();
-    Countly.track_pageview(url);
   }));
 
   store.subscribeAction(({ type, payload }) => {
     if (type !== 'modals/open') return;
     matomo.trackEvent('modal', 'open', payload.name);
-    Countly.add_event({ key: `modals/open/${payload.name}` });
   });
 };
