8af205eaf38d393a2d03b851739af8872615619c
Denis Davidyuk <denis_davidyuk@hotmail.com>
2019-06-27 22:50:22 +0200

    Set proper Matomo side id on master branch

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,6 @@
 require('./scripts/label-icons');
 const path = require('path');
+const branch = require('./scripts/current-branch');
 
 const parseBool = val => (val ? JSON.parse(val) : false);
 
@@ -7,6 +8,10 @@ const parseBool = val => (val ? JSON.parse(val) : false);
 const { IS_MOBILE_DEVICE, IS_PWA, npm_package_version } = process.env;
 const IS_CORDOVA = parseBool(process.env.IS_CORDOVA);
 const UNFINISHED_FEATURES = parseBool(process.env.UNFINISHED_FEATURES);
+
+if (branch === 'master') {
+  process.env.VUE_APP_MATOMO_SITE_ID = 2;
+}
 
 module.exports = {
   publicPath: IS_CORDOVA ? './' : '/',
