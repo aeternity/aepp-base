e43cb371dbc5ac0564c8faa3e6baebdb426921ca
Denis Davidyuk <denis_davidyuk@hotmail.com>
2019-07-01 17:00:43 +0200

    Show notificationMnemonicBackup on account creation

--- src/store/plugins/ui/index.js
+++ src/store/plugins/ui/index.js
@@ -3,7 +3,6 @@ import connectionStatusTracker from './connectionStatusTracker';
 import desktopGuide from './desktopGuide';
 import ledgerConnection from './ledgerConnection';
 import modals from './modals';
-import notificationForMnemonicBackup from './notificationForMnemonicBackup';
 import notificationOnRemoteConnection from './notificationOnRemoteConnection';
 import observables from './observables';
 import unlockWalletIfNotEncrypted from './unlockWalletIfNotEncrypted';
@@ -15,7 +14,6 @@ export default store => [
   ...process.env.IS_MOBILE_DEVICE
     ? [
       browserPathTracker,
-      notificationForMnemonicBackup,
       notificationOnRemoteConnection,
       unlockWalletIfNotEncrypted,
     ]
