100d416883ab187585ec9947aff25b3fcdbda9c1
Denis Davidyuk <denis_davidyuk@hotmail.com>
2019-07-06 01:13:59 +0200

    AccountInline: Use formatAddress filter

--- src/components/AccountInline.vue
+++ src/components/AccountInline.vue
@@ -1,24 +1,23 @@
 <template>
-  <span class="account-inline">
-    <AeIdenticon :address="address" />{{ ' ' }}
-    <template v-if="name">
-      {{ name }}
-    </template>
-    <AeAddress
-      v-else
-      :address="address"
-      length="short"
-    />
+  <span
+    v-copy-on-click="address"
+    class="account-inline"
+    :class="{ address: !name }"
+  >
+    <AeIdenticon :address="address" />
+    {{ name ? name : formatAddress(address, 'short') }}
   </span>
 </template>
 
 <script>
 import { mapState } from 'vuex';
+import formatAddress from '../filters/formatAddress';
+import copyOnClick from '../directives/copyOnClick';
 import AeIdenticon from './AeIdenticon.vue';
-import AeAddress from './AeAddress.vue';
 
 export default {
-  components: { AeIdenticon, AeAddress },
+  components: { AeIdenticon },
+  directives: { copyOnClick },
   props: {
     address: { type: String, required: true },
   },
@@ -27,12 +26,30 @@ export default {
       return get(this.address);
     },
   }),
+  methods: { formatAddress },
 };
 </script>
 
 <style lang="scss" scoped>
-.account-inline .ae-identicon {
-  vertical-align: middle;
-  height: 1.055em;
+@import '../styles/variables/typography.scss';
+@import './address-copied.scss';
+
+.account-inline {
+  &.address {
+    font-family: $font-mono;
+  }
+
+  &.v-copied {
+    @extend %address-copied;
+
+    &:before {
+      content: 'copied';
+    }
+  }
+
+  .ae-identicon {
+    vertical-align: middle;
+    height: 1.055em;
+  }
 }
 </style>
