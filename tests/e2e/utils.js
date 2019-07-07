0b0e6fbeccf43d7bbadaf3b70b0546de8c1d427d
Denis Davidyuk <denis_davidyuk@hotmail.com>
2019-07-07 04:39:58 +0200

    Remove unnecessary reseting of service workers

--- tests/e2e/utils.js
+++ tests/e2e/utils.js
@@ -1,8 +1,4 @@
 // eslint-disable-next-line import/prefer-default-export
 export const prepare = async () => {
   delete localStorage.vuex;
-
-  if (!navigator.serviceWorker) return;
-  (await navigator.serviceWorker.getRegistrations())
-    .map(registration => registration.unregister());
 };
