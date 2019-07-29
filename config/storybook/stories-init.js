c20d56e732d247f05c569a201abd891975409604
Denis Davidyuk <denis_davidyuk@hotmail.com>
2019-07-29 06:11:01 +0200

    storybook: Fix i18n

--- config/storybook/stories-init.js
+++ config/storybook/stories-init.js
@@ -1,4 +1,3 @@
-import '../../src/store/plugins/ui/veeValidate';
 import '../../src/components/icon.scss';
 import Vue from 'vue';
 import Vuex from 'vuex';
@@ -6,6 +5,12 @@ import VueRx from 'vue-rx';
 
 Vue.use(Vuex);
 Vue.use(VueRx);
-Vue.prototype.$globals = {
-  IS_MOBILE_DEVICE: true,
-};
+Object.assign(Vue.prototype, {
+  $globals: {
+    IS_MOBILE_DEVICE: true,
+  },
+  $i18n: {},
+});
+
+Vue.prototype.$i18n = require('../../src/store/plugins/ui/languages').i18n;
+require('../../src/store/plugins/ui/veeValidate');
