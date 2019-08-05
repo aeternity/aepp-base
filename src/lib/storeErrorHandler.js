01516161ddac2421eebe3b8705d15b1aa5a54417
Dmitry Kostin <etharner@gmail.com>
2019-08-05 20:25:46 +1000

    Raise add prompt after clicking button

--- src/lib/storeErrorHandler.js
+++ src/lib/storeErrorHandler.js
@@ -1,15 +1,13 @@
 import Vue from 'vue';
-import StoreLoadError from '../pages/StoreLoadError.vue';
-import { i18n } from '../store/plugins/ui/languages';
 
-window.onerror = function errorHandler() {
+window.onerror = async function errorHandler() {
   if (document.getElementById('app').innerHTML) {
     window.onerror = null;
     return;
   }
 
+  const StoreLoadError = (await import('../pages/StoreLoadError.vue')).default;
   new Vue({
-    i18n,
     render: h => h(StoreLoadError),
   }).$mount('#app');
 };
