3fa926ed133fabe9ba21497a69b144c202d6e6fc
Denis Davidyuk <denis_davidyuk@hotmail.com>
2019-08-15 17:06:03 +0200

    Apps: More precise scss import

--- src/pages/mobile/Apps.vue
+++ src/pages/mobile/Apps.vue
@@ -88,7 +88,7 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-@import '../../styles/placeholders/typography.scss';
+@import '../../styles/globals/functions.scss';
 @import '../../styles/variables/colors.scss';
 
 .apps {
