f3c075084eab07f7bac67d45d8ad03ff0504e1c6
Denis Davidyuk <denis_davidyuk@hotmail.com>
2019-08-21 08:58:27 +0200

    Show desktop guide only if not ledger account used

--- src/store/plugins/ui/desktopGuide.js
+++ src/store/plugins/ui/desktopGuide.js
@@ -2,9 +2,9 @@ import { i18n } from './languages';
 
 export default (store) => {
   store.watch(
-    (state, { loggedIn }) => loggedIn,
-    async (loggedIn) => {
-      if (!loggedIn || !store.state.desktop.showGuideOnStartup) return;
+    ({ accounts: { list } }) => list.some(({ source: { type } }) => type !== 'ledger'),
+    async (hasNotLedgerAccounts) => {
+      if (!hasNotLedgerAccounts || !store.state.desktop.showGuideOnStartup) return;
       await store.dispatch('modals/open', {
         name: 'alert',
         text: i18n.t('guide-desktop'),
