199dc595a282ccf004fdd02c2475e39c67d66165
Nikita Cedrik <nikita.cedrik@gmail.com>
2019-09-30 12:48:08 +1000

    initSdk: Allow aepp to query Base aepp version

--- src/store/plugins/initSdk.js
+++ src/store/plugins/initSdk.js
@@ -81,6 +81,7 @@ export default (store) => {
         name: 'readQrCode',
         title: title || i18n.t('scan-qr-code'),
       }),
+      baseAppVersion: () => process.env.npm_package_version,
     };
 
     let sdkActive = false;
@@ -111,7 +112,7 @@ export default (store) => {
             };
           },
           methods,
-          deepConfiguration: { Ae: { methods: ['readQrCode'] } },
+          deepConfiguration: { Ae: { methods: ['readQrCode', 'baseAppVersion'] } },
         },
       )({
         url: network.url,
