22518a7eea557c35367c2ac7d20e876db6d41c46
Denis Davidyuk <denis_davidyuk@hotmail.com>
2019-10-08 07:26:00 +0200

    Remove AddToHomeScreenPrompt

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,5 @@
 import Router from 'vue-router';
 import store from '../store';
-import AddToHomeScreenPrompt from '../pages/mobile/AddToHomeScreenPrompt.vue';
 
 store.subscribe((mutation, state) => {
   switch (mutation.type) {
@@ -22,24 +21,13 @@ export default (async () => {
     },
   });
 
-  if (
-    process.env.IS_MOBILE_DEVICE && !process.env.IS_CORDOVA
-    && !process.env.IS_PWA && !process.env.IS_IOS
-    && process.env.NODE_ENV === 'production'
-  ) {
-    router.addRoutes([{
-      path: '/',
-      component: AddToHomeScreenPrompt,
-    }]);
-  } else {
-    await Promise.all([
-      process.env.IS_MOBILE_DEVICE
-        ? import(/* webpackChunkName: "ui-mobile" */ './routes/mobile')
-        : import(/* webpackChunkName: "ui-desktop" */ './routes/desktop'),
-      import('./routes/common'),
-    ]
-      .map(async module => router.addRoutes((await module).default)));
-  }
+  await Promise.all([
+    process.env.IS_MOBILE_DEVICE
+      ? import(/* webpackChunkName: "ui-mobile" */ './routes/mobile')
+      : import(/* webpackChunkName: "ui-desktop" */ './routes/desktop'),
+    import('./routes/common'),
+  ]
+    .map(async module => router.addRoutes((await module).default)));
 
   store.watch(
     (state, { loggedIn }) => loggedIn,
