fba54b6f25e28100ec1d35fc885d25b020d6f57e
Denis Davidyuk <denis_davidyuk@hotmail.com>
2019-10-10 11:43:40 +0200

    Show metadata from web manifests for featured apps

--- src/lib/appsRegistry.js
+++ src/lib/appsRegistry.js
@@ -1,35 +1,13 @@
-import blockchainExplorerIcon from '../assets/icons/aepps/blockchain-explorer.svg';
-import tokenMigrationIcon from '../assets/icons/aepps/token-migration.jpg';
-import graffitiAeppIcon from '../assets/icons/aepps/graffiti-aepp.svg';
-
 export { default as DEFAULT_ICON } from '../assets/icons/aepps/default.svg';
 
-export const aeternityApps = [{
-  name: 'Graffiti',
-  path: 'graffiti.aeternity.com',
-  icon: graffitiAeppIcon,
-}, {
-  name: 'æternity Voting',
-  path: 'aeternity.com/aepp-hybrid-voting',
-  icon: blockchainExplorerIcon,
-}, {
-  name: 'Token Migration',
-  path: 'token-migration.aepps.com',
-  icon: tokenMigrationIcon,
-}, ...process.env.NODE_ENV === 'production' ? [] : [{
-  name: 'Example æpp',
-  path: 'example-aepp.origin.aepps.com',
-  icon: blockchainExplorerIcon,
-}, {
-  name: 'Middleware æpp',
-  path: 'mdw.aepps.com',
-  icon: blockchainExplorerIcon,
-}, {
-  name: 'Naming æpp example',
-  path: 'aeternity.com/aepp-naming-example',
-  icon: blockchainExplorerIcon,
-}, {
-  name: 'Faucet æpp',
-  path: 'faucet.aepps.com',
-  icon: blockchainExplorerIcon,
-}]];
+export const aeternityAppsPaths = [
+  'graffiti.aeternity.com',
+  'aeternity.com/aepp-hybrid-voting',
+  'token-migration.aepps.com',
+  ...process.env.NODE_ENV === 'production' ? [] : [
+    'example-aepp.origin.aepps.com',
+    'mdw.aepps.com',
+    'aeternity.com/aepp-naming-example',
+    'faucet.aepps.com',
+  ],
+];
