d4478e79d54405239b264ecb5bc0c14f9b110a3f
Denis Davidyuk <denis_davidyuk@hotmail.com>
2019-10-11 09:22:40 +0200

    Remove hybrid voting aepp
    https://devchat.aeternity.com/group/aepp-base?msg=e55132ec-b010-46bd-ba48-86d58e42abfb

--- src/lib/appsRegistry.js
+++ src/lib/appsRegistry.js
@@ -2,7 +2,6 @@ export { default as DEFAULT_ICON } from '../assets/icons/aepps/default.svg';
 
 export const aeternityAppsPaths = [
   'graffiti.aeternity.com',
-  'aeternity.com/aepp-hybrid-voting',
   'token-migration.aepps.com',
   ...process.env.NODE_ENV === 'production' ? [] : [
     'example-aepp.origin.aepps.com',
