ee8e9547a7a8bdddbcfae2680aff68919d2e8e00
Denis Davidyuk <denis_davidyuk@hotmail.com>
2019-10-11 13:59:19 +0200

    initSdk: Simplify using fetchJson

--- src/store/plugins/initSdk.js
+++ src/store/plugins/initSdk.js
@@ -1,5 +1,6 @@
 import { get, isEqual } from 'lodash-es';
 import { handleUnknownError, isNotFoundError } from '../../lib/utils';
+import { fetchJson } from '../utils';
 import { i18n } from './ui/languages';
 
 export default (store) => {
@@ -122,7 +123,7 @@ export default (store) => {
         axiosConfig: { errorHandler },
       }),
       (async () => {
-        const swag = await (await fetch(`${network.middlewareUrl}/middleware/api`)).json();
+        const swag = await fetchJson(`${network.middlewareUrl}/middleware/api`);
         return Swagger.compose({
           methods: {
             urlFor: path => network.middlewareUrl + path,
