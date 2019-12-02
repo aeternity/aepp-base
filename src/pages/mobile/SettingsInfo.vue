28a08f588d0fe101647e2929d425a392c0d981ce
Denis Davidyuk <denis_davidyuk@hotmail.com>
2019-12-02 10:14:42 +0100

    SettingsInfo: Combine subscriptions into a single one

--- src/pages/mobile/SettingsInfo.vue
+++ src/pages/mobile/SettingsInfo.vue
@@ -22,8 +22,8 @@
 </template>
 
 <script>
-import { pick } from 'lodash-es';
-import { mapGetters } from 'vuex';
+import { pick, get } from 'lodash-es';
+import { mapState } from 'vuex';
 import MobilePage from '../../components/mobile/Page.vue';
 import AeCard from '../../components/AeCard.vue';
 import ListItem from '../../components/ListItem.vue';
@@ -34,45 +34,35 @@ export default {
     AeCard,
     ListItem,
   },
-  data: () => ({
-    nodeVersion: null,
-    compilerVersion: null,
-  }),
   subscriptions() {
     return pick(this.$store.state.observables, ['middlewareStatus']);
   },
-  computed: {
-    ...mapGetters(['currentNetwork']),
-    infoFields() {
+  computed: mapState({
+    infoFields({ sdk }, { currentNetwork }) {
       return [{
         name: this.$t('settings.info.version'),
         value: process.env.npm_package_version,
       }, {
         name: this.$t('settings.info.node-version'),
-        value: this.nodeVersion,
+        value: get(sdk, 'selectedNode.version'),
       }, {
         name: this.$t('settings.info.compiler-version'),
-        value: this.compilerVersion,
+        value: get(sdk, 'compilerVersion'),
       }, {
         name: this.$t('settings.info.compiler-url'),
-        subtitle: this.currentNetwork.compilerUrl,
+        subtitle: currentNetwork.compilerUrl,
       }, {
         name: this.$t('settings.info.sdk-version'),
         value: process.env.SDK_VERSION,
       }, {
         name: this.$t('settings.info.middleware-url'),
-        subtitle: this.currentNetwork.middlewareUrl,
+        subtitle: currentNetwork.middlewareUrl,
       },
       ...this.middlewareStatus.OK ? [] : [{
         name: this.$t('settings.info.blocks-in-queue'),
         value: this.middlewareStatus.queueLength,
       }]];
     },
-  },
-  async mounted() {
-    if (this.$store.state.sdk.then) await this.$store.state.sdk;
-    this.compilerVersion = this.$store.state.sdk.compilerVersion;
-    this.nodeVersion = this.$store.state.sdk.selectedNode.version;
-  },
+  }),
 };
 </script>
