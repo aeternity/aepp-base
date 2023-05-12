ff0f4d0b2e4cf253361f7543bb07fd431927fcc5
Denis Davidyuk <denis_davidyuk@hotmail.com>
2023-05-12 18:08:24 +0600

    test: store local screenshots separately

--- cypress.config.js
+++ cypress.config.js
@@ -10,6 +10,9 @@ module.exports = defineConfig({
   fixturesFolder: 'tests/e2e/fixtures',
   screenshotsFolder: 'tests/e2e/screenshots',
   videosFolder: 'tests/e2e/videos',
+  env: process.env.CI === 'true' ? {} : {
+    pluginVisualRegressionImagesPath: '{spec_path}/__image_snapshots_local__',
+  },
   e2e: {
     specPattern: 'tests/e2e/specs/**/*.cy.{js,jsx,ts,tsx}',
     supportFile: 'tests/e2e/support/index.js',
