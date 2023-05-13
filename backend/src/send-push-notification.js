217ee4b36e82ec64d8b9ca3ea295425a35663bf4
Denis Davidyuk <denis_davidyuk@hotmail.com>
2023-05-13 11:27:12 +0600

    chore: cleanup backend files

--- backend/src/send-push-notification.js
+++ backend/src/send-push-notification.js
@@ -2,9 +2,9 @@ import webPush from 'web-push';
 
 export default async (pushApiSubscription) => {
   webPush.setVapidDetails(
-    process.env.VAPID_SUBJECT,
-    process.env.VAPID_PUBLIC_KEY,
-    process.env.VAPID_PRIVATE_KEY,
+    'https://github.com/aeternity/aepp-base/issues',
+    process.env.VAPID_PUBLIC_KEY ?? 'BHkQhNWW2TKfKfxo7vAgXkZGcVOXGrjhIZJlN1hKp6abIjWJgO8FYPswXJ35XEuKw46O9yZ-8KmsZ4-TXNBePcw',
+    process.env.VAPID_PRIVATE_KEY ?? 'mwSZbWF_yU_h1gXlqlBy9cOfkOF-Pa0jhHxbTFXdhFE',
   );
   if (pushApiSubscription !== 'not-available') {
     const subscription = JSON.parse(pushApiSubscription);
