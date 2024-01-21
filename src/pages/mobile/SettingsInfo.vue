363c62a1b20347442270b390b4c2dafeb9445052
Denis Davidyuk <denis_davidyuk@hotmail.com>
2024-01-22 00:28:58 +0700

    chore(settings): add links to status endpoints as in SW

--- src/pages/mobile/SettingsInfo.vue
+++ src/pages/mobile/SettingsInfo.vue
@@ -12,6 +12,7 @@
         :key="idx"
         :subtitle="field.subtitle || ''"
         :title="field.name"
+        :to="field.to"
       >
         <template slot="right">
           {{ field.value }}
@@ -41,13 +42,15 @@ export default {
     infoFields({ sdk }, { currentNetwork }) {
       return [{
         name: this.$t('settings.info.version'),
-        value: process.env.VUE_APP_VERSION,
+        value: `${process.env.VUE_APP_VERSION} (${process.env.VUE_APP_REVISION.slice(0, 7)})`,
+        to: `https://github.com/aeternity/aepp-base/releases/tag/v${process.env.VUE_APP_VERSION}`,
       }, {
         name: this.$t('settings.info.sdk-version'),
         value: process.env.VUE_APP_SDK_VERSION,
       }, {
         name: this.$t('settings.info.node.url'),
         subtitle: currentNetwork.url,
+        to: `${currentNetwork.url}/v3/status`,
       }, {
         name: this.$t('settings.info.node.version'),
         value: sdk?.selectedNode?.version,
@@ -57,6 +60,7 @@ export default {
       }, {
         name: this.$t('settings.info.middleware.url'),
         subtitle: currentNetwork.middlewareUrl,
+        to: `${currentNetwork.middlewareUrl}/status`,
       }, {
         name: this.$t('settings.info.middleware.version'),
         value: this.middlewareStatus?.mdwVersion,
