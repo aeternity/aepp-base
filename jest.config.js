efdfb09fac8d4d583e87c07df374304626c0d02e
Denis Davidyuk <denis_davidyuk@hotmail.com>
2025-02-22 22:44:23 +0100

    chore(deps): remove unused `swagger-client` package

--- jest.config.js
+++ jest.config.js
@@ -17,14 +17,6 @@ module.exports = {
   transformIgnorePatterns: [`node_modules/(?!(${packagesToTranspile.join('|')})/)`],
   moduleNameMapper: {
     '^.*\\.svg\\?icon-component$': '<rootDir>/config/jest/EmptySvg.vue',
-    // https://github.com/jestjs/jest/issues/10422
-    '^@swagger-api/apidom-reference/parse/parsers/binary$':
-      '@swagger-api/apidom-reference/cjs/parse/parsers/binary/index-browser.cjs',
-    '^@swagger-api/apidom-reference/(.*)/strategies/openapi-3-1$':
-      '@swagger-api/apidom-reference/cjs/$1/strategies/openapi-3-1/index.cjs',
-    '^@swagger-api/apidom-reference/dereference/strategies/openapi-3-1/selectors/(.*)$':
-      '@swagger-api/apidom-reference/cjs/dereference/strategies/openapi-3-1/selectors/$1/index.cjs',
-    '^@swagger-api/apidom-reference/(.*)$': '@swagger-api/apidom-reference/cjs/$1.cjs',
   },
   snapshotSerializers: ['jest-serializer-vue'],
   testMatch: ['**/tests/unit/**/*.spec.(js|jsx|ts|tsx)', '**/__tests__/*.(js|jsx|ts|tsx)'],
